b19ecc74123657cd4497eea71d57b7fa15c3024f
alexover1 <alex@overstreet.me>
2021-05-16 23:13:00 -0400

    Test

---
+++ stat/src/index.ts
@@ -0,0 +1,27 @@
+import { ApolloServer, gql } from "apollo-server-express";
+import express from "express";
+
+const app = express();
+
+const typeDefs = gql`
+  type Query {
+    hello: String!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => "What's up";
+  }
+}
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+})
+
+server.applyMiddleware( { app});
+
+app.listen(4000, () => {
+  console.log("Server ready!");
+})
