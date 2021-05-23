0e0c8451c97b785d5994c54ff23df1202d9ceed3
alexover1 <alex@overstreet.me>
2021-05-22 21:21:04 -0400

    a

--- next/tailwind.config.js
+++ next/tailwind.config.js
@@ -2,6 +2,7 @@ module.exports = {
   purge: [
     "./src/pages/**/*.{js,jsx,ts,tsx}",
     "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/**/*.{js,ts,jsx,tsx}",
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
