f18dacad55039d6106625b298ae25400f65499b8
alexover1 <alex@overstreet.me>
2021-05-26 22:29:42 -0400

    fix(web): og?

--- next/src/components/common/OpenGraph.tsx
+++ next/src/components/common/OpenGraph.tsx
@@ -11,11 +11,11 @@ export const OpenGraph: React.FC<{
       <title>{title}</title>
       <meta name="description" content={description} />
 
-      <meta property="og:url" content="https://capsule.app/" />
-      <meta property="og:type" content="website" />
-      <meta property="og:title" content={title} />
-      <meta property="og:description" content={description} />
-      <meta property="og:image" content={image} />
+      <meta name="og:url" content="https://capsule.app/" />
+      <meta name="og:type" content="website" />
+      <meta name="og:title" content={title} />
+      <meta name="og:description" content={description} />
+      <meta name="og:image" content={image} />
 
       <meta name="twitter:card" content="summary" />
       <meta property="twitter:domain" content="capsule.app" />
