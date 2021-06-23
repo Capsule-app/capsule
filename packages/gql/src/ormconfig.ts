696443b213d412e00f8093050232ac2200bf58bf
Alex Overstreet <alex@overstreet.me>
2021-06-22 20:45:59 -0500

    fix(gql): remove useless pg-connection-string

--- packages/gql/src/ormconfig.ts
+++ packages/gql/src/ormconfig.ts
@@ -1,22 +1,16 @@
 import dotenv from "dotenv";
 import { ConnectionOptions } from "typeorm";
-import { parse } from "pg-connection-string";
 import { checkEnv } from "./checkEnv";
 
 if (process.env.NODE_ENV !== "production") {
   dotenv.config();
 }
-checkEnv();
 
-const config = parse(process.env.DATABASE_URL);
+checkEnv();
 
 export const ormconfig: ConnectionOptions = {
   type: "postgres",
-  host: config.host,
-  port: Number(config.port) || 5432,
-  username: config.user,
-  password: config.password,
-  database: config.database,
+  url: process.env.DATABASE_URL,
   synchronize: true,
   entities: [`${__dirname}/entity/*.*`],
 };
